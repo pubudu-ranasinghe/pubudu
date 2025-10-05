251a8ceec63bbd97d9e744b842d64b62402e8c2e
Pubudu Ranasinghe <pubudutr@gmail.com>
2025-10-05 17:28:49 +0530

    feat: update dependencies and add new features
    - Added new dependencies for React, Tailwind CSS, and Astro integrations.
    - Introduced an Open Graph image for better social media sharing.
    - Created a robots.txt file to allow all crawlers and specify sitemap location.
    - Enhanced BlogLayout to include keywords and publish date for articles.
    - Updated Layout to support structured data for SEO, including JSON-LD for articles and personal information.
    - Refactored styles in Layout and BlogLayout for improved readability and maintainability.
    - Configured TypeScript to use React JSX syntax.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site: 'https://pubuduranasinghe.com',
+  integrations: [mdx(), sitemap(), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
